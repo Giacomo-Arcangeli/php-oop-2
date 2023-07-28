6b8d62a8216253dd81fb12261c8b4659892eb740
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-07-28 16:22:53 +0200

    add category and product

---
+++ Models/products/product.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/../Category.php';
+
+class Product{
+
+    private $name;
+    private $category;
+
+    public function __construct($name , $category){
+
+        $this->setName($name);
+        $this->setCategory($category);
+    }
+
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function setName($name):self{
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getCategory(){
+        return $this->category;
+    }
+
+    public function setCategory(Category $category):self{
+        if (&category instanceof Category) $this->category = $category;
+        return $this;
+    }
+
+
+}
